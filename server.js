0b3ebd67605f398d37c8dbfd79960710ce845cb6
Sbrisbri <sabrina.morelli@mail.polimi.it>
2022-11-23 13:19:56 +0100

    connectioncolor

--- server.js
+++ server.js
@@ -21,5 +21,11 @@ let io = serverSocket(server);
 io.on("connection", newConnection); //secondo parametro che dice quando una nuova connessione avviene deve fare qualcosa il server
 
 function newConnection(newSocket){
-console.log(newSocket);
+console.log(newSocket.id);
+newSocket.on("mouse", mouseReceived);
+
+function mouseReceived(dataReceived){
+    console.log(dataReceived);
+    newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+}
 }
